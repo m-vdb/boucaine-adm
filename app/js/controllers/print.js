31ce111990e48debe759514d21c18d4292b33e5c
mvergerdelbove <mvergerdelbove@work4labs.com>
2014-12-02 23:53:44 -0800

    we know how to create codes

--- app/js/controllers/print.js
+++ app/js/controllers/print.js
@@ -1,7 +1,19 @@
 module.exports = function ($scope, Restangular, growl) {
   $scope.value = 1;
   $scope.amount = 10;
+  $scope.codes = [];
+
   $scope.generateCodes = function () {
-    console.log("amount", $scope.amount, "value", $scope.value);
-  }
+    _.times($scope.amount, createCode);
+  };
+
+  function createCode () {
+    var data = {number: $scope.value, type: 'pizza'};
+    Restangular.all("codes").post(data).then(function (obj) {
+      $scope.codes.push(obj);
+      console.log(obj);
+    }, function () {
+      growl.error("Une erreur inconnue s'est produite.");
+    });
+  };
 };
