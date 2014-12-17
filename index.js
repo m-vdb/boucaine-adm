50554b7260f89b97ff2322da81d9fc77a3f295c1
mvergerdelbove <mvergerdelbove@work4labs.com>
2014-12-17 08:24:29 +0100

    add jade + auth

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
+var express = require('express'),
+    session = require('cookie-session'),
+    jade = require('jade');
+
 var path = require("path"),
-    port = process.env.PORT || 3000;
+    port = process.env.PORT || 3000,
+    secret = process.env.SECRET || "local",
+    public = path.join(process.cwd(), 'public');
 
-var root = path.join(process.cwd(), 'public');
-var express = require('express');
+// config
 var app = express();
+app.engine('.html', jade.__express);
+app.engine('.jade', jade.__express);
+app.use(session({keys: [secret]}));
+app.use("/public", express.static(public));
 
+// views
+app.get("/", auth, function (req, res) {
+  res.render('index.html');
+});
+app.get("/auth", function (req, res) {
+  res.render('auth.jade');
+});
+
+// listen
 var server = app.listen(port, function () {
-
   var host = server.address().address
   var port = server.address().port
-
   console.log('Listening at http://%s:%s', host, port)
 });
 
-app.use(express.static(root));
+
+// auth middelware
+function auth (req, res, next) {
+  if (!req.session.uid) {
+    res.redirect('/auth');
+  }
+  else {
+    next();
+  }
+}
